2aeb4c18004e3458aa28e835fb76c153eb58bc73
sonnsei <6518512@qq.com>
2023-06-17 12:00:03 +0800

    init

---
+++ tuling/src/main/java/com/zhouyu/service/ConversionService.java
@@ -0,0 +1,19 @@
+package com.zhouyu.service;
+
+import org.springframework.context.weaving.LoadTimeWeaverAware;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author Sonnsei
+ * @Date 2023/6/15 11:30 PM
+ * Have a great day!
+ **/
+@Component
+public class ConversionService implements LoadTimeWeaverAware {
+	@Override
+	public void setLoadTimeWeaver(LoadTimeWeaver loadTimeWeaver) {
+
+	}
+}
